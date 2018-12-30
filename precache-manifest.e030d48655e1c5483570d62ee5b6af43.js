ea28d0ea4aa143f1bc0eae97d9c5f02529556ca2
Jun Zheng <me@jackzh.com>
2018-12-30 14:33:02 -0600

    publish: feature: Add deployment script
    generated from commit a2ad3dd9c50f16d5cc2ddb32a0f871add721ad86

---
+++ precache-manifest.e030d48655e1c5483570d62ee5b6af43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "58266fa73323d43b8e6b",
+    "url": "/static/js/main.58266fa7.chunk.js"
+  },
+  {
+    "revision": "799a4dcbd87e6df6599a",
+    "url": "/static/js/1.799a4dcb.chunk.js"
+  },
+  {
+    "revision": "799a4dcbd87e6df6599a",
+    "url": "/static/css/1.949f9403.chunk.css"
+  },
+  {
+    "revision": "9a099199696de3be2fbd2e309b099611",
+    "url": "/index.html"
+  }
+];
